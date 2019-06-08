9c45f061f934c12e2a0efd1e416e904c5b451714
Quan Nguyen <quangineer@gmail.com>
2019-06-08 15:36:33 -0700

    main file to analyze

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pandas as pd 
 
 
-# df = pd.read_csv("test.csv")
-# print (df)
+df_test = pd.read_csv("test.csv")
 
-df = pd.read_csv("train.csv")
-print (df)
+
+df_train = pd.read_csv("train.csv")
+
+
+print (list(df_test.columns))
+print (list(df_train.columns))

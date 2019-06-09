6f498820e78d669ec852a813bb655ad5e972652a
Quan Nguyen <quangineer@gmail.com>
2019-06-09 14:54:31 -0700

    updates

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns
 
-
-df_test = pd.read_csv("test.csv")
-
+# df_test = pd.read_csv("test.csv")
+# print (list(df_test.columns))
 
 df_train = pd.read_csv("train.csv")
-
-
-print (list(df_test.columns))
 print (list(df_train.columns))
+
